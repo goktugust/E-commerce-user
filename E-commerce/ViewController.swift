666d04d76ac9a6aeeed12ced3a14db15741ee924
Göktuğ Üstüner <g.ustunerr@gmail.com>
2021-04-29 12:37:45 +0300

    Fixed constraints. Add textfield images.

--- E-commerce/ViewController.swift
+++ E-commerce/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func signInPressed(_ sender: UIButton) {
+        
+        
+    }
     
+    @IBAction func signUpPressed(_ sender: UIButton) {
+        
+    }
 }
 
