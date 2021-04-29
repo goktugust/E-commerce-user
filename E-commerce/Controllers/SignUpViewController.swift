181941f0c10e98c678dc43352a75ca63e3f5cb84
Göktuğ Üstüner <g.ustunerr@gmail.com>
2021-04-29 13:37:42 +0300

    add pods and firebase

---
+++ E-commerce/Controllers/SignUpViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.swift
+//  E-commerce
+//
+//  Created by Göktuğ Üstüner on 29.04.2021.
+//
+
+import UIKit
+import Firebase
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func doneBtnPressed(_ sender: UIButton) {
+        
+        
+        
+        if let email = emailText.text, let password = passwordText.text{
+            
+            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+                if let e = error {
+                    print(e.localizedDescription)
+                }else{
+                    print("succesfuly signed")
+                }
+            }
+        }
+        
+        
+        
+    }
+    
+}
